262a12e185236849f731adbf3c58fe88fc24364e
AndreM132 <61509420+AndreM132@users.noreply.github.com>
2020-06-03 18:30:48 +0100

    Files Courseware Exercise
    Teams

---
+++ Code/Filesexercise.py
@@ -0,0 +1,14 @@
+teams = open("teams.txt", "w")
+for n in range(1): 
+    newLine = "Arsenal" + str() + "\n"
+    teams.write(newLine)
+    newLine = "Liverpool" + str() + "\n"
+    #teams.write(newLine)
+    newLine = "Man City" + str() + "\n"
+    #teams.write(newLine)
+    newLine = "Chelsea" + str() + "\n"
+    teams.write(newLine)
+    newLine = "Man U" + str() + "\n"
+    #teams.write(newLine)
+
+teams.close()
